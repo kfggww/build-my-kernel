986ca6f827b851c4596fd41f1ab237bb2bd71358
junan <kfggw@outlook.com>
2021-03-11 23:25:07 +0800

    basic char device driver

--- ldd/chapter3/scull.c
+++ ldd/chapter3/scull.c
@@ -11,43 +11,59 @@ struct scull_qset {
 };
 
 struct scull_dev {
-  int quantum;
-  int qset;
-  struct scull_qset *data;
+  long max_size;
+  void *data_block;
   struct cdev cdev;
 };
 
 int scull_open(struct inode *inode, struct file *filp)
 {
-	struct scull_dev *dev; /* device information */
-
-	dev = container_of(inode->i_cdev, struct scull_dev, cdev);
-	filp->private_data = dev; /* for other methods */
-	printk(KERN_ALERT "open has been called");
-      	return 0;          /* success */
+  struct scull_dev *scull_device;
+  scull_device = container_of(inode->i_cdev, struct scull_dev, cdev);
+  filp->private_data = scull_device;
+  printk(KERN_ALERT "my_scull_device has been opened");
+  return 0;
 }
 
 int scull_release(struct inode *inode, struct file *filp)
 {
-	return 0;
+  printk(KERN_ALERT "my_scull_device has been released");
+  return 0;
 }
 
-ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
-                loff_t *f_pos)
+ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
 {
-  printk(KERN_ALERT "read has been called");
+  struct scull_dev* scull_device = filp->private_data;
+
+  // the end
+  if(*f_pos >= scull_device->max_size)
+    return 0;
+
+  long rest = scull_device->max_size - *f_pos;
+  count = count <= rest ? count : rest;
+  unsigned long err = copy_to_user(buf, scull_device->data_block + *f_pos, count);
+  
+  (*f_pos) += count;
   return count;
 }
 
 
-ssize_t scull_write(struct file *filp, const char __user *buf, size_t count,
-                loff_t *f_pos)
+ssize_t scull_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
 {
-  printk(KERN_ALERT "write has been called");
+  struct scull_dev *scull_device = filp->private_data;
+  // the end
+  if(*f_pos >= scull_device->max_size)
+    return 0;
+
+  long rest = scull_device->max_size - *f_pos;
+  count = count <= rest ? count : rest;
+  unsigned long err = copy_from_user(scull_device->data_block + *f_pos, buf, count);
+  
+  (*f_pos) += count;
   return count;
 }
 
-struct scull_dev *scull_device;
+struct scull_dev *my_scull_device;
 int major;
 int minor;
 
@@ -61,46 +77,44 @@ struct file_operations scull_fops = {
 
 static int scull_init_module(void)
 {
-  // 申请设备号
-  dev_t dev = 0;
-  int result = alloc_chrdev_region(&dev, 0, 1, "scull");
-  if(result < 0) {
-    printk(KERN_ALERT "获取Major失败!");
-    return result;
+  // alloc device id
+  dev_t dev_id;
+  int err = alloc_chrdev_region(&dev_id, 0, 1, "scull");
+  if(err < 0) {
+    printk(KERN_ALERT "alloc_chrdev_region Failed");
+    return err;
   }
+  major = MAJOR(dev_id);
+  minor = MINOR(dev_id);
 
-  major = MAJOR(dev);
-  minor = MINOR(dev);
+  // alloc scull_device
+  my_scull_device = kmalloc(sizeof(struct scull_dev), GFP_KERNEL);
+  cdev_init(&my_scull_device->cdev, &scull_fops);
+  my_scull_device->cdev.owner = THIS_MODULE;
 
-  // 创建scull设备
-  scull_device = kmalloc(sizeof(struct scull_dev), GFP_KERNEL);
-  if(scull_device == 0) {
-    result = -ENOMEM;
-    return result;
-  }
-
-  // 初始化scull设备
-  cdev_init(&scull_device->cdev, &scull_fops);
-  scull_device->cdev.owner = THIS_MODULE;
-  scull_device->cdev.ops = &scull_fops;
-  cdev_add(&scull_device->cdev, dev, 1);
-  printk(KERN_ALERT "scull_init");
+  // alloc data_block space
+  my_scull_device->max_size = 4096;
+  my_scull_device->data_block = kmalloc(4096, GFP_KERNEL);
+  
+  // add scull_device->cdev to kernel
+  cdev_add(&my_scull_device->cdev, dev_id, 1);
+  printk(KERN_ALERT "my_scull_device has been added to kernel");
   return 0;
 }
 
-static void scull_cleanup_module(void)
+static void scull_exit_module(void)
 {
-  dev_t devno = MKDEV(major, minor);
-  if(scull_device != 0) {
-    cdev_del(&scull_device->cdev);
-    kfree(scull_device);
-  }
-  printk(KERN_ALERT "scull_cleanup");
-  unregister_chrdev_region(devno, 1);
+  // release dev id
+  dev_t dev_id = MKDEV(major, minor);
+  unregister_chrdev_region(dev_id, 1);
+
+  // delete cdev
+  cdev_del(&my_scull_device->cdev);
+  printk(KERN_ALERT "my_scull_deivce has been removed from kernel");
 }
 
 
 module_init(scull_init_module);
-module_exit(scull_cleanup_module);
+module_exit(scull_exit_module);
 
 MODULE_LICENSE("GPL v3");
