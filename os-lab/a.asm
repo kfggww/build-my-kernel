ad5836f75901fb41bd95a7a737c4b32da3cf629d
kfggww <kfggww@outlook.com>
2021-06-15 22:19:55 +0800

    在屏幕显示字符.

--- os-lab/a.asm
+++ os-lab/a.asm
@@ -1,7 +1,25 @@
+    ;; 使用显卡的文本模式在屏幕上输出信息
 
+    mov ax, 0xb800
+    mov ds, ax
+    mov ax, 0x7c0
+    mov es, ax
 
-    mov ax,0xff
-    mov bx,0xfe
-    mov cx,0xfd
-    mov dx,0xfc
+    mov cx, msg_len
+    mov bx, 0
+    mov si, 0
+
+show:
+    mov dl, [es:msg+si]
+    mov [bx], dl
+    mov byte [bx+1], 0x84
+    add bx, 2
+    inc si
+    loop show
+
+msg:
+    db "Hello, MBR!"
+
+msg_len equ $ - msg
+
     hlt
